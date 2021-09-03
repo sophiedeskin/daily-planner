bd72c8e5e0774fa638a25ee034fdb511cb89192a
Sophie Deskin <sophiedeskin@gmail.com>
2021-09-03 09:14:15 -0700

    updating javascript and readme file

--- logic.js
+++ logic.js
@@ -1,27 +1,94 @@
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("currentDay").innerHTML = m + "/" + d + "/" + y;
+var activitiesInput = document.querySelector("textarea");
+var activities = []
+const data = JSON.parse(localStorage.getItem("day-schedule")) || {};
 
-var saveButton = document.querySelector("button")
-var activities = document.querySelector("textarea")
-var hour9 = document.querySelector("#hour-9")
-var hour10 = document.querySelector("#hour-10")
-var hour11 = document.querySelector("#hour-11")
-var hour12 = document.querySelector("#hour-12")
+const date = new Date();
+    const hour = date.getHours();
 
 
-saveButton.addEventListener("click", function(event) {
+
+var parsed9 = JSON.parse(localStorage.getItem("9"));
+$("#text9").val(parsed9.text);
+
+var parsed10 = JSON.parse(localStorage.getItem("10"));
+$("#text10").val(parsed10.text);
+
+var parsed11 = JSON.parse(localStorage.getItem("11"));
+$("#text11").val(parsed11.text);
+
+var parsed12 = JSON.parse(localStorage.getItem("12"));
+$("#text12").val(parsed12.text);
+
+var parsed13 = JSON.parse(localStorage.getItem("13"));
+$("#text13").val(parsed13.text);
+
+var parsed14 = JSON.parse(localStorage.getItem("14"));
+$("#text14").val(parsed14.text);
+
+var parsed15 = JSON.parse(localStorage.getItem("15"));
+$("#text15").val(parsed15.text);
+
+var parsed16 = JSON.parse(localStorage.getItem("16"));
+$("#text16").val(parsed16.text);
+
+var parsed17 = JSON.parse(localStorage.getItem("17"));
+$("#text17").val(parsed17.text);
+
+// Find all .time-block elements
+$(".time-block").each(function (i, object) {
+const hour = object.id;
+if(hour === date.getHours()) {
+//     //current hour
+document.getElementById(`text${object.id}`).style.backgroundColor = "red"
+}else if (hour > date.getHours()){
+     document.getElementById(`text${object.id}`).style.backgroundColor = "green"
+//     //future hour
+ }else {
+//     //past hour
+ document.getElementById(`text${object.id}`).style.backgroundColor = "grey"
+}
+
+
+
+    // see if you have any data in local storage for each text area
+    if (object.id) {
+      // place that text
+      console.log(object.id);
+    }
+    // apply the class for if the time block is current, in the past, or future
+  });
+
+  $(".saveBtn").on("click", function (event) {
     event.preventDefault();
-const data = [{
-    hour9: activities.value.trim(),
-    hour10: activities.value.trim(),
-    hour11: activities.value.trim(),
-    hour12: activities.value.trim(),
-}];
-localStorage.setItem("data", JSON.stringify(data));
-});
+    // Retrieve the value from the text area
+    const val = $(this).siblings("textarea").eq(0).val().trim();
+    // retrieve the id from the parent
+    const id = $(this).parent().attr("id");
+    console.log(val)
+    console.log(id)
+    const data = {time:id, text:val}
+    // save to localstorage
+    localStorage.setItem(id, JSON.stringify(data));
+    console.log(data)
+  });
+    
+
+  setInterval(function () {
+    document.getElementById("currentDay").innerHTML = formatDate(new Date());
+  }, 1000);
+  
+  function formatDate(today) {
+    var year = today.getFullYear(),
+      month = today.getMonth() + 1,
+      date = today.getDate(),
+      hour = today.getHours(),
+      minutes = today.getMinutes(),
+      seconds = today.getSeconds();
+  
+    return `${month}/${date}/${year} ${hour}:${minutes}:${seconds}`;
+  }
+
+
 
 
 
